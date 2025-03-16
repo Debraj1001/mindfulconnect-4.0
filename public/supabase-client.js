eaa4e6166f41a3dd1c038d75c796829320e0e9d1
Debraj Pradhan <p.pradhan3172@gmail.com>
2025-03-16 20:17:32 +0530

    Update supabase-client.js

--- public/supabase-client.js
+++ public/supabase-client.js
@@ -1,4 +1,4 @@
-import { createClient } from "@supabase/supabase-js"
+import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
 
 // Initialize Supabase client
 const supabaseUrl = "https://qeuppsmpoyhectbluhfv.supabase.co" // Replace with your actual Supabase URL
